8c0bdd82c147cf618b4275ea6239892755670c4a
Jonas <jonasvange@gmail.com>
2021-12-01 13:55:02 +0100

    Update House.js
    Co-Authored-By: Joel Funk Persson <16280266+Jolpango@users.noreply.github.com>

--- js/House.js
+++ js/House.js
@@ -40,9 +40,14 @@ export default class HousePar {
         this.xp = (this.xp + account.influence) + 1;
 		if(this.xp >= this.xpNext){
 			this.currentSize = this.currentSize + 1;
-			this.xpNext = this.xpRates[this.currentSize];
+			this.xpNext = this.xpRates[this.currentSize + 1];
 			this.currentHouse = this.houseSize[this.currentSize];
 		}
+        if (this.xp < this.xpRates[this.currentSize - 1]){
+            this.currentSize = this.currentSize - 1;
+            this.xpNext = this.xpRates[this.currentSize - 1];
+            this.currentHouse = this.houseSize[this.currentSize];
+        }
     }
 
     updateImage(){
