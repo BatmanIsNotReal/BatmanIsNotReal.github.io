46f67f4daa53d09afb7cccae8025e1b5833c0cf8
Jonas <jonasvange@gmail.com>
2021-12-16 15:54:28 +0100

    a

--- js/events/EventHandler.js
+++ js/events/EventHandler.js
@@ -19,22 +19,50 @@ export default class EventHandler{
         }
     }
 
-    getRandomEvent(document){
+    getRandomEvent(){
+        const container = document.getElementById("modal-event");
+        removeAllChildren(container);
+        
         let len = Object.keys(this.events).length;
         console.log(len);
         let rand = this.getRandomInt(1, Object.keys(this.events).length);
         console.log(rand);
-        let event = this.events[1].text; //add rand when done testing.
-        //Check which button is pressed first
-        this.events[1].conOne();
-        this.events[1].conTwo();
+        
+        const container = document.getElementById("modal-event");
+        const header = document.createElement("h3");
+        header.innerHTML = this.events[rand].header;
+
+        const text = document.createElement("p");
+        text.innerHTML = this.events[rand].text;
+
+        const optionOne = document.createElement("button");
+        optionOne.innerHTML = this.events[rand].optionOne;
+
+        const optionTwo = document.createElement("button");
+        optionTwo.innerHTML = this.events[rand].optionTwo;
+
+        container.appendChild(header);
+        container.appendChild(text);
+        container.appendChild(optionOne);
+        container.appendChild(optionTwo);
+
+        optionOne.addEventListener("click", () => {this.events[rand].conOne();});
+        optionTwo.addEventListener("click", () => {this.events[rand].conTwo();});
+
         console.log("event happened");
-        alert(event);
+        
     }
 
     getRandomInt(min, max){
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
+    }
+}
+
+
+function removeAllChildren(element) {
+    while (element.children.length > 0) {
+        element.removeChild(element.children[element.children.length - 1]);
     }
 }
