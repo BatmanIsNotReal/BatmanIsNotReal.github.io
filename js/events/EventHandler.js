56c7a88f18c7e376f83c86acb03eccd739764002
Jonas <jonasvange@gmail.com>
2021-12-16 11:55:54 +0100

    a

--- js/events/EventHandler.js
+++ js/events/EventHandler.js
@@ -8,7 +8,7 @@ export default class EventHandler{
     }
 
     getRandomEvent(document){
-        var event = this.events[this.getRandomInt(0, Object.keys(this.events).length)].text;
+        var event = this.events[Object.keys(this.events).length].text;
         console.log("event happened");
         alert(event);
     }
