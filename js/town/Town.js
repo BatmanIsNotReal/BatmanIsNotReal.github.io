15e0ecfed8220f183f342a95160c85a6fef0e105
Jonas <jonasvange@gmail.com>
2023-11-16 21:06:42 +0100

    Update Town.js

--- js/town/Town.js
+++ js/town/Town.js
@@ -196,8 +196,8 @@ export default class Town{
                 console.log(id);
                 let image = document.createElement('img');
                 image.setAttribute("id", id);
-                image.setAttribute("width", 32);
-                image.setAttribute("height", 32);
+                image.setAttribute("width", 16);
+                image.setAttribute("height", 16);
                 image.setAttribute("class", "tile");
                 g.appendChild(image);
             }
