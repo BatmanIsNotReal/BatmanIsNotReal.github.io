e57d222196769b9dc9bfc3a8f7f33592ba6a5b85
Jonas <jonasvange@gmail.com>
2024-12-30 00:53:46 +0100

    Update MusicHandler.js

--- js/MusicHandler.js
+++ js/MusicHandler.js
@@ -2,7 +2,7 @@ export default class MusicHandler{
     constructor(){
        
 
-        this.buyVampire = new Audio('music\sounds\strigoiBought.mp3');
+        this.buyVampire = new Audio('https://github.com/BatmanIsNotReal/BatmanIsNotReal.github.io/blob/e667297a116dc6971f72c825c29d008549b62549/music/sounds/strigoiBought.mp3');
     }
 
     
